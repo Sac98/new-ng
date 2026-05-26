72671bed057bb624b47006cf1c60d65971e0d387
sachin <sacbukkigar98@gmail.com>
2026-05-26 16:38:48 +0530

    Added formbuilder service and setValues method

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, ɵInternalFormsSharedModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ɵInternalFormsSharedModule } from '@angular/forms';
 
 @Component({
   selector: 'reactive',
@@ -10,24 +10,56 @@ import { FormControl, FormGroup, ReactiveFormsModule, ɵInternalFormsSharedModul
 })
 export class ReactiveFormsComponent {
   reactiveForm: FormGroup
-  constructor() {
-    this.reactiveForm = new FormGroup({
-      firstName: new FormControl(''),
-      lastName: new FormControl(''),
-      email: new FormControl(''),
-      password: new FormControl(),
-      check: new FormControl(true),
-      address: new FormGroup({
-        city: new FormControl(),
-        street: new FormControl(),
-        pincode: new FormControl()
+  constructor(private fb: FormBuilder) {
+    // Using FormControl and FormGroup
+    // this.reactiveForm = new FormGroup({
+    //   firstName: new FormControl(''),
+    //   lastName: new FormControl(''),
+    //   email: new FormControl(''),
+    //   password: new FormControl(),
+    //   check: new FormControl(true),
+    //   address: new FormGroup({
+    //     city: new FormControl(),
+    //     street: new FormControl(),
+    //     pincode: new FormControl()
+    //   })
+    // })
+
+    // Using FormBuilder
+    this.reactiveForm = fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      password: [''],
+      check: [true],
+      address: fb.group({
+        city: [''],
+        street: [''],
+        pincode: ['']
       })
     })
   }
   onSubmit() {
     console.log(this.reactiveForm.value);
   }
-  onReset(){
+  onReset() {
     this.reactiveForm.reset();
+  }
+  setAllValues() {
+    let userObject = {
+      firstName: "rahul",
+      lastName: "verma",
+      email: "rahul.verma@example.com",
+      password: "password123",
+      check: true,
+      address: {
+        city: "New York",
+        street: "123 Main St",
+        pincode: "10001"
+      }
+    };
+    this.reactiveForm.setValue(userObject);
+    console.log(this.reactiveForm.value);
+
   }
 }
